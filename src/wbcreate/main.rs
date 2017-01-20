9fe0d2b5689e507765b2ea39102161fd3de2be9e
Akira Hayakawa <ruby.wktk@gmail.com>
2017-01-20 13:17:58 +0900

    wbcreate: moved to clap
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/wbcreate/main.rs
+++ src/wbcreate/main.rs
@@ -1,34 +1,63 @@
+extern crate clap;
 extern crate getopts;
 extern crate lib;
 
 use std::process::Command;
 use std::env;
+use clap::{Arg, App};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut opts = getopts::Options::new();
-    opts.optopt("", "writeback_threshold", "todo", "INT");
-    opts.optopt("", "nr_max_batched_writeback", "todo", "INT");
-    opts.optopt("", "update_sb_record_interval", "todo", "INT");
-    opts.optopt("", "sync_data_interval", "todo", "INT");
-    opts.optopt("", "read_cache_threshold", "todo", "INT");
-    opts.optflag("", "write_around_mode", "todo");
-    opts.optflag("", "reformat", "Reformat the caching device. This cleans up all existing cache blocks");
-    opts.optflag("h", "help", "todo");
-    let matches = opts.parse(&args[1..]).expect("Couldn't parse args");
+    let matches = App::new("wbcreate")
+        .version("0.1")
+        .author("Akira Hayakawa <ruby.wkkt@gmail.com>")
+        .about("Create a writeboost device")
+        .arg(Arg::with_name("LVNAME")
+             .help("name of the writeboost device")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("BACKINGDEV")
+             .help("path to the backing device")
+             .required(true)
+             .index(2))
+        .arg(Arg::with_name("CACHEDEV")
+             .help("path to the caching dev")
+             .required(true)
+             .index(3))
+        .arg(Arg::with_name("reformat")
+             .help("Reformat the caching device. This cleans up all existing cache blocks")
+             .long("reformat"))
+        .arg(Arg::with_name("write_around_mode")
+             .long("write_around_mode"))
+        .arg(Arg::with_name("writeback_threshold")
+             .long("writeback_threshold")
+             .value_name("INT")
+             .takes_value(true))
+        .arg(Arg::with_name("nr_max_batched_writeback")
+             .long("nr_max_batched_writeback")
+             .value_name("INT")
+             .takes_value(true))
+        .arg(Arg::with_name("update_sb_record_interval")
+             .long("update_sb_record_interval")
+             .value_name("INT")
+             .takes_value(true))
+        .arg(Arg::with_name("sync_data_interval")
+             .long("sync_data_interval")
+             .value_name("INT")
+             .takes_value(true))
+        .arg(Arg::with_name("read_cache_threshold")
+             .long("read_cache_threshold")
+             .value_name("INT")
+             .takes_value(true))
+        .get_matches();
 
-    if matches.free.len() != 3 {
-        panic!("Too much or less essential parameters (should be two)");
-    }
-
-    let wbname = matches.free[0].clone();
+    let wbname = matches.value_of("LVNAME").unwrap().to_string();
     let backing_dev = {
-         let name = matches.free[1].clone();
+         let name = matches.value_of("BACKINGDEV").unwrap().to_string();
          lib::BlockDevice::new(name)
     };
-    let caching_dev_name = matches.free[2].clone();
+    let caching_dev_name = matches.value_of("CACHEDEV").unwrap().to_string();
 
-    if matches.opt_present("reformat") {
+    if matches.is_present("reformat") {
         Command::new("dd")
             .arg("if=/dev/zero")
             .arg(format!("of={}", caching_dev_name))
@@ -39,7 +68,7 @@ fn main() {
     }
 
     let mut optionals: Vec<String> = Vec::new();
-    if matches.opt_present("write_around_mode") {
+    if matches.is_present("write_around_mode") {
         optionals.push("write_around_mode".to_string());
         optionals.push("1".to_string());
     }
@@ -50,10 +79,10 @@ fn main() {
         "sync_data_interval",
         "read_cache_threshold"];
     for name in &tunables {
-        match matches.opt_str(name) {
+        match matches.value_of(name) {
             Some(value) => {
                 optionals.push(name.to_string());
-                optionals.push(value);
+                optionals.push(value.to_string());
             }
             _ => {}
         }
