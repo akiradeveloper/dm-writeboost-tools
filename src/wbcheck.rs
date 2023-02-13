05c7dfda16df8bd332aae8a8dd83d4ab2830f39e
Akira Hayakawa <ruby.wktk@gmail.com>
2023-02-13 10:53:09 +0900

    Fix wbcheck

--- src/wbcheck.rs
+++ src/wbcheck.rs
@@ -1,17 +1,27 @@
 extern crate clap;
 extern crate crc;
 
-use clap::{App, Arg};
 use std::fs::File;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
-use std::str::FromStr;
 
 use crc::{Crc, CRC_32_ISCSI};
 pub const CASTAGNOLI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
 fn checksum(data: &[u8]) -> u32 {
     CASTAGNOLI.checksum(data)
+}
+
+use clap::Parser;
+#[derive(Parser)]
+#[command(name = "wbcheck")]
+#[command(about = "Check if the segment is broken")]
+#[command(author, version)]
+struct Args {
+    #[arg(help = "Path to the cache device")]
+    cachedev: String,
+    #[arg(help = "Segment id")]
+    segid: i32,
 }
 
 #[test]
@@ -21,27 +31,10 @@ fn test_checksum() {
 }
 
 fn main() {
-    let matches = App::new("wbcheck")
-        .version(lib::VERSION)
-        .author(lib::AUTHOR)
-        .about("Check if the segment is broken")
-        .arg(
-            Arg::with_name("CACHEDEV")
-                .help("Name of the cache device")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("SEGID")
-                .help("Segment id")
-                .required(true)
-                .index(2),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let devname: String = matches.value_of("CACHEDEV").unwrap().to_string();
-    let id: i32 =
-        i32::from_str(matches.value_of("SEGID").unwrap()).expect("Segment id should be int");
+    let devname: String = args.cachedev;
+    let id = args.segid;
     let cache_dev = lib::CacheDevice::new(devname.to_owned());
 
     let mut f = File::open(&devname).expect(&format!("Device {} not found", &devname));
