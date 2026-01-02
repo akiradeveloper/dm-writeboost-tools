9051532f2ded388a81e7e147b55f5b174a8e46bb
Akira Hayakawa <ruby.wktk@gmail.com>
2026-01-02 12:36:03 +0900

    Add uninit test file to test check command

--- writeboost-cli/src/sub/check/mod.rs
+++ writeboost-cli/src/sub/check/mod.rs
@@ -11,6 +11,56 @@ fn checksum(data: &[u8]) -> u32 {
     CASTAGNOLI.checksum(data)
 }
 
+#[derive(thiserror::Error, Debug)]
+enum CheckError {
+    #[error("segment is not initialized")]
+    NotInitialized,
+    #[error(
+        "segment {seg_id} is broken. checksum: computed={computed_checksum}, expected={expected_checksum}"
+    )]
+    CacheBlockBroken {
+        seg_id: i32,
+        computed_checksum: u32,
+        expected_checksum: u32,
+    },
+}
+
+fn do_check(devname: &str, seg_id: i32) -> Result<(), CheckError> {
+    let cache_dev = CacheDevice::new(devname.to_owned());
+
+    let mut f = File::open(&devname).expect(&format!("Device {} not found", &devname));
+
+    let start_byte: u64 = (cache_dev.calc_segment_start(seg_id) as u64) << 9;
+    f.seek(SeekFrom::Start(start_byte)).unwrap();
+
+    let header = {
+        let mut buf = vec![0; 512];
+        f.read(&mut buf).unwrap();
+        SegmentHeader::from_buf(&buf)
+    };
+
+    if header.uninitialized() {
+        return Err(CheckError::NotInitialized);
+    }
+
+    let computed = {
+        let size: usize = (4096 - 512) + ((header.length as usize) << 12);
+        let mut buf = vec![0; size];
+        f.read(&mut buf).unwrap();
+        checksum(&buf)
+    };
+
+    if computed != header.checksum {
+        return Err(CheckError::CacheBlockBroken {
+            seg_id,
+            computed_checksum: computed,
+            expected_checksum: header.checksum,
+        });
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,6 +69,22 @@ mod tests {
     fn test_checksum() {
         let buf = vec![0; 4096 - 512];
         assert_eq!(checksum(&buf), 143703573);
+    }
+
+    #[test]
+    fn test_check() {
+        let devname = "data/sample.cache.226";
+        let seg_id = 1;
+        let res = do_check(&devname, seg_id);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_uninitialized() {
+        let devname = "data/sample.cache.uninit";
+        let seg_id = 0;
+        let res = do_check(&devname, seg_id);
+        assert!(matches!(res, Err(CheckError::NotInitialized)));
     }
 }
 
@@ -34,34 +100,16 @@ pub struct CommandArgs {
 pub fn run(args: CommandArgs) {
     let devname: String = args.cachedev;
     let id = args.segid;
-    let cache_dev = CacheDevice::new(devname.to_owned());
 
-    let mut f = File::open(&devname).expect(&format!("Device {} not found", &devname));
-
-    let start_byte: u64 = (cache_dev.calc_segment_start(id) as u64) << 9;
-    f.seek(SeekFrom::Start(start_byte)).unwrap();
-
-    let header = {
-        let mut buf = vec![0; 512];
-        f.read(&mut buf).unwrap();
-        SegmentHeader::from_buf(&buf)
-    };
-
-    if header.uninitialized() {
-        std::process::exit(0);
-    }
-
-    let computed = {
-        let size: usize = (4096 - 512) + ((header.length as usize) << 12);
-        let mut buf = vec![0; size];
-        f.read(&mut buf).unwrap();
-        checksum(&buf)
-    };
-
-    if computed != header.checksum {
-        panic!(
-            "Checksum is broken. computed={}, expected={}",
-            computed, header.checksum
-        );
+    match do_check(&devname, id) {
+        Ok(()) => {}
+        Err(CheckError::NotInitialized) => {
+            // Since segments are zero-ed out at formatting,
+            // if the segment is all zeros, it is considered still unused.
+            eprintln!("segment is not initialized");
+        }
+        Err(e) => {
+            panic!("{e}")
+        }
     }
 }
